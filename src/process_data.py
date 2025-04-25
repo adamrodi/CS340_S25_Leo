6dc5dc4553ba5f10998c23f8d8b4adba7a2c6d9b
maxoftheriver <max.delrio@selu.edu>
2025-04-24 21:42:13 -0500

    Update process_data.py

--- src/process_data.py
+++ src/process_data.py
@@ -180,8 +180,13 @@ def calc_yearly_volume_avg(fileName):
     except Exception as e:
         raise e # propagate the error up the call stack to be handled in main.py
     #
-def local_max_min():
+def local_max_min(x1,x2):
     pass
+    #
+
+def percentage_increase():
+    pass
+    #
 #
 
 #%% SELF-RUN               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
